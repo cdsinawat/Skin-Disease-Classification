0bd8e3669a87fd511c1004638bc5db22b69ffeff
cdsinawat <srinawatj@gmail.com>
2022-12-15 14:44:58 +0700

    1

---
+++ app.py
@@ -0,0 +1,285 @@
+from lobe import ImageModel
+import os
+import shutil
+import random
+import numpy as np 
+from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import streamlit as st
+from datetime import datetime
+import time
+import torch
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import seaborn as sns
+import random
+from PIL import Image
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras import Sequential, Model
+from keras.layers import (Conv2D, MaxPooling2D, Dense, Flatten, \
+                          Dropout, Input,GlobalAveragePooling2D,BatchNormalization)
+from tensorflow.keras.activations import softmax
+from tensorflow.keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from mpl_toolkits.axes_grid1 import ImageGrid
+from keras.models import Model
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.metrics import (ConfusionMatrixDisplay, confusion_matrix, \
+                             accuracy_score)
+import cv2
+
+
+
+
+our_folders = ['Acne and Rosacea Photos', \
+              'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions', \
+              'Melanoma Skin Cancer Nevi and Moles']
+root_dir = '/home/cdsinawat/Downloads/nsc-skin-20221212T015152Z-001/nsc-skin/datasets/test'
+
+test_dir = '/home/cdsinawat/Downloads/nsc-skin-20221212T015152Z-001/nsc-skin/datasets/test'
+
+acne_train_path = os.path.join(root_dir,'Acne and Rosacea Photos')
+actinic_train_path = os.path.join(root_dir, 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions')
+melonama_train_path = os.path.join(root_dir, 'Melanoma Skin Cancer Nevi and Moles')
+acne_test_path = os.path.join(test_dir,'Acne and Rosacea Photos')
+actinic_test_path = os.path.join(test_dir, 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions')
+melonama_test_path = os.path.join(test_dir, 'Melanoma Skin Cancer Nevi and Moles')
+
+actinic_test_files = ([files_ for _, _, files_ in os.walk(actinic_test_path)])[0]
+acne_test_files = ([files_ for _, _, files_ in os.walk(acne_test_path)])[0]
+melonama_test_files = ([files_ for _, _, files_ in os.walk(melonama_test_path)])[0]
+
+train_dirs = []
+for i in our_folders:
+    for folder_,_, files_ in os.walk(f'datasets/train/{i}'):
+        print(folder_)
+        train_dirs.append(folder_)
+
+
+actinic_train_files = ([files_ for _, _, files_ in os.walk(actinic_train_path)])[0]
+acne_train_files = ([files_ for _, _, files_ in os.walk(acne_train_path)])[0]
+melonama_train_files = ([files_ for _, _, files_ in os.walk(melonama_train_path)])[0]
+
+
+def plotGridImages(d_name, list_files, train_path,nrows= 1, ncols=5):
+    # for folder_name in our_folders:
+    fig = plt.figure(1, figsize=(30, 30))
+    grid = ImageGrid(fig, 111, nrows_ncols=(nrows, ncols), axes_pad=0.05)
+    print(f"{d_name}")
+    for i, img_id in enumerate(random.sample(list_files,ncols)):
+        ax = grid[i]
+        image_dir_path = os.path.join(train_path, img_id)
+        img = image.load_img(image_dir_path, (224, 224))
+        img = image.img_to_array(img)
+        ax.imshow(img / 255.)
+        ax.text(20, 200, 'LABEL: %s' % d_name, color='k', backgroundcolor='w',\
+        alpha=0.8)
+        ax.axis('off')
+    # plt.tight_layout()
+    plt.show()
+
+final_df = pd.DataFrame()
+melonama_df = pd.DataFrame()
+melonama_df['Image'] = [melonama_train_path+'/'+img for img in melonama_train_files]
+melonama_df['Label'] = "melonama"
+
+actinic_df = pd.DataFrame()
+actinic_df['Image'] =  [actinic_train_path+'/'+img for img in actinic_train_files]
+actinic_df['Label'] = "actinic"
+
+acne_df = pd.DataFrame()
+acne_df['Image'] =  [acne_train_path+'/'+img for img in acne_train_files]
+acne_df['Label'] = "acne"
+
+final_df = final_df.append([actinic_df, melonama_df, acne_df])
+
+
+final_test_df = pd.DataFrame()
+################# melonama #########
+
+melonama_test_df = pd.DataFrame()
+melonama_test_df['Image'] = [melonama_test_path+'/'+img for img in melonama_test_files]
+melonama_test_df['Label'] = "melonama"
+
+##################  actinic  ##########
+
+actinic_test_df = pd.DataFrame()
+actinic_test_df['Image'] =  [actinic_test_path+'/'+img for img in actinic_test_files]
+actinic_test_df['Label'] = "actinic"
+
+################  acne  ##########
+
+acne_test_df = pd.DataFrame()
+acne_test_df['Image'] =  [acne_test_path+'/'+img for img in acne_test_files]
+acne_test_df['Label'] = "acne"
+###########################################
+###########################################
+
+final_test_df = final_test_df.append([actinic_test_df, 
+                                      melonama_test_df, 
+                                      acne_test_df])
+
+
+train_data_gen  = ImageDataGenerator(
+                                    rescale=1 / 255.0,
+                                    rotation_range=40,
+                                    width_shift_range=0.2,
+                                    height_shift_range=0.2,
+                                    horizontal_flip = True,
+                                    vertical_flip = True,
+                                    validation_split=0.2,
+                                    fill_mode='nearest')
+test_data_gen = ImageDataGenerator(rescale=1 / 255.0)
+
+batch_size = 8
+train_generator = train_data_gen.flow_from_dataframe(
+    dataframe=final_df,
+    x_col="Image",
+    y_col="Label",
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode="categorical",#sparse
+    subset='training',
+    shuffle=True,
+    seed=42
+)
+valid_generator = train_data_gen.flow_from_dataframe(
+    dataframe=final_df,
+    x_col="Image",
+    y_col="Label",
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode="categorical", #sparse
+    subset='validation',
+    shuffle=True,
+    seed=42 
+)
+test_generator = test_data_gen.flow_from_dataframe(
+    dataframe=final_test_df,
+    x_col="Image",
+    y_col="Label",
+    target_size=(224, 224),
+    batch_size=1,
+    class_mode='categorical',
+    shuffle=False,
+)
+
+# VGG16 with Input shape of our Images
+# Include Top is set to false to allow us to add more layers
+from tensorflow.keras.applications import Xception
+res = Xception(weights ='imagenet', include_top = False, 
+               input_shape = (224, 224, 3)) 
+               
+# Setting the trainable to false
+res.trainable = False
+
+
+x= res.output
+x = GlobalAveragePooling2D()(x)
+x = BatchNormalization()(x)
+# x = Dropout(0.5)(x) 
+x = Dense(512, activation ='relu')(x)
+x = BatchNormalization()(x)
+# x = Dropout(0.5)(x)
+
+x = Dense(256, activation ='relu')(x)
+x = BatchNormalization()(x)
+
+x = Dense(3, activation ='softmax')(x)
+model = Model(res.input, x)
+
+model.compile(optimizer =tf.keras.optimizers.Adam(learning_rate=0.001),  #'Adam'
+              loss ="categorical_crossentropy",  #sparse_categorical_crossentropy
+              metrics =["categorical_accuracy"])  #sparse_categorical_accuracy
+
+model.summary()
+
+custom_early_stopping = EarlyStopping(
+    monitor='val_loss', 
+    patience=10, 
+    min_delta=0.001, 
+    mode='min'
+)
+
+history = model.fit(train_generator, 
+                    epochs=1, 
+                    validation_data=valid_generator,
+                   callbacks=[custom_early_stopping])
+
+
+
+def imageInput(device, src):
+    
+    if src == 'อัปโหลดรูปภาพ':
+        image_file = st.file_uploader("ตรวจสอบรูปภาพ", type=['png', 'jpeg', 'jpg'])
+        col1, col2 = st.columns(2)
+        if image_file is not None:
+            img = Image.open(image_file)
+            with col1:
+                st.image(img, caption='รูปภาพที่นำเข้ามา', use_column_width='always')
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
+            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
+
+            #call Model prediction--
+            
+         
+            
+            img = tf.keras.utils.load_img(
+                imgpath,
+                target_size=(224, 224)
+            )
+            img_array = tf.keras.utils.img_to_array(img)
+            img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+            predictions = model.predict(img_array)
+            score = tf.nn.softmax(predictions[0])
+
+            score = tf.nn.softmax(predictions[0])
+
+            class_names = list(train_generator.class_indices)
+
+            st.write(
+                "This image most likely belongs to {} with a {:.2f} percent confidence."
+                .format(class_names[np.argmax(score)], 100 * np.max(score))
+            )
+
+
+                   
+                    
+            #--Display predicton
+            #++++++++++++++++++++++++++++++++++++++
+            # img_ = Image.open(outputpath)
+            # with col2:
+            #     st.image(img_, caption='ผลลัพธ์จากการตรวจสอบ', use_column_width='always')
+
+
+def main():
+    # -- Sidebar
+    st.sidebar.title('Ai Health Check Skin Disease')
+    datasrc = st.sidebar.radio("เลือกประเภทรูปแบบการนำเข้า", ['อัปโหลดรูปภาพ'])
+    
+        
+                
+    option = st.sidebar.radio("ระบุประเภทข้อมูล", ['Image'], disabled = False)
+    if torch.cuda.is_available():
+        deviceoption = st.sidebar.radio("ประมวลผลโดยใช้", ['cpu', 'cuda'], disabled = False, index=1)
+    else:
+        deviceoption = st.sidebar.radio("ประมวลผลโดยใช้", ['cpu', 'cuda'], disabled = True, index=0)
+    # -- End of Sidebar
+
+    st.header('Ai Health Check Skin-Disease')
+    
+    if option == "Image":    
+        imageInput(deviceoption, datasrc)
+
+if __name__ == '__main__':
+  
+    main()
+       
